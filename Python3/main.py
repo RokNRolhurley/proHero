8518b85db90fc4d0a0f00dfcdd5e2557f101bf40
Sakil <mahbub.qa7@gmail.com>
2024-08-18 21:31:49 -0400

    Test-milestone

---
+++ Python3/main.py
@@ -0,0 +1,25 @@
+import  math 
+#import randint, randrange, math, random, sammple, choice  
+
+# print(math.sin(90))
+# print(math.pi)
+
+#def sin(x):
+
+    # if 2 * x == math.pi:
+    #     return 0.9999
+    # else:
+    #     return None
+
+    # math.pi = 3.14 
+
+    # print(sin(pi/2))
+    # print(math.sin(math.pi/2)) 
+
+#print(random)
+
+# students=["Anne", "Sarah", "Tila" ]
+# print(choice(students))
+
+x = math.ceil(math.pi)
+print(x)
---
+++ Python3/test.py
@@ -0,0 +1,20 @@
+secret_number = 100 
+guess = int(input('Guess the secret number: '))
+while guess != secret_number:
+    guess = int(input('Guess the secret number: '))
+else:
+    print('Congrats, you found it')
+
+
+
+
+# Age = int(input ("Enter your age: "))
+# if Age > 18:
+#     print("You are an adult")
+# else :
+#     print("You are not an adult")
+
+
+# print ("Hello World", "\n this is a test")
+# print 
+
