888df7b7187981ee1564d11249c19a12641d80e6
Sakil <mahbub.qa7@gmail.com>
2026-02-12 10:14:56 -0500

    module37

---
+++ 6thMilestone/module37/noatation.js
@@ -0,0 +1,19 @@
+const person = {
+    name: "Brad", 
+    age: 22,
+    friends: ["Rambo", "Eminem"],
+    country: "USA",
+    family: { 
+        father: "John", 
+        mother: "Jane", 
+        sister: "Emily"
+    },
+    10:"secret code",
+    "hero-name": "Batman"
+}
+
+
+console.log(person.name);
+
+console.log(person["10"]);
+
---
+++ 6thMilestone/module37/shortcuts.js
@@ -0,0 +1,15 @@
+
+const test = '0';
+// test?console.log('its truthy'):console.log('its not falsy');
+
+const num = 50;
+// num>=10 && num<100 ? console.log('between 10 and 100'):console.log('outside the range');
+
+const isActive = true;
+
+// const showUser = () => console.log('display user in ui');
+
+// const hideUser = () => console.log('hide user in ui');
+
+// isActive ? showUser() : hideUser();
+
