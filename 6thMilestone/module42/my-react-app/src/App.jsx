00fc9652d222552b13cc38f59436cccf01209b01
Sakil Mahbub <95638905+RokNRolhurley@users.noreply.github.com>
2026-04-13 09:51:26 -0400

    Commit

--- 6thMilestone/module42/my-react-app/src/App.jsx
+++ 6thMilestone/module42/my-react-app/src/App.jsx
@@ -4,7 +4,7 @@ import AvailablePlayers from './Components/AvailablePlayers/AvailablePlayers'
 import SelectedPlayers from './Components/SelectedPlayers/SelectedPlayers'
 import Navbar from './Components/Navbar/Navbar'
 import Banner from './Components/Banner/Banner'
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 
 
 const fetchplayers = async() => {
@@ -14,27 +14,36 @@ const fetchplayers = async() => {
   };
 
 function App() {
-
+  const [toggle, setToggle] = useState(true);
   const playerPromise = fetchplayers();  
 
   return (
     <>
    
     <Navbar></Navbar>
-    
+   
+    <div className="max-w-[1200px] mx-auto flex justify-between items-center">
+      
+      <div className=' justify-between items-center '>
+          <span className="font-bold text-lg">Available Players</span>
+          <button onClick={()=>setToggle(true)} className={`py-3 px-3 border-1 border-gray-400 rounded-l-2xl border-r-0 ${toggle === true ? 'bg-[#E7FE29]': ''} `}>Available</button>
+          <button onClick={()=>setToggle(false)} className={`py-3 px-3 border-1 border-gray-400 rounded-r-2xl border-l-0 ${toggle === false ? 'bg-[#E7FE29]': ''} `}>Selected<span> (0)</span></button>
+      </div>
+    </div>
 
-    <Banner></Banner>
-    <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
+    {
+      toggle ===true? <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
         <AvailablePlayers playerPromise={playerPromise}></AvailablePlayers>
-    </Suspense>
-    <SelectedPlayers></SelectedPlayers>
+    </Suspense> : <SelectedPlayers></SelectedPlayers>
+    }
     
-    <section id="center">
-        <Counter  ></Counter>
-    </section>
-
+    
+    
+    
     </>
   )
 }
 
-export default App
+
+
+export default App;
