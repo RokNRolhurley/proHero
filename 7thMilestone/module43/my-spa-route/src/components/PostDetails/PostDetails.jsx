9a12bf9708db4f3b66b1e3357c1111146e750ce3
Sakil Mahbub <sakilmahbub@gmail.com>
2026-08-11 17:09:44 -0400

    module43 Updates

--- 7thMilestone/module43/my-spa-route/src/components/PostDetails/PostDetails.jsx
+++ 7thMilestone/module43/my-spa-route/src/components/PostDetails/PostDetails.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { useLoaderData, useNavigate } from 'react-router';
 
 const PostDetails = () => {
     const post = useLoaderData();
     const { id, title, body } = post;
+    const navigate = useNavigate();
+
+    const handleRetrun = () =>{
+        navigate(-1)
+    }
     
+    const postStyle = {
+        
+        border: '1px solid grey',
+        margin: '10px',
+        padding: '10px',
+        borderRadius: '5px',
+        color:   'purple',
+        textAlign: 'left',
+    }
+
      return (
-        <div>
+        <div style={postStyle}>
             <h3>{post.title}</h3>
             <p>{post.body}</p>
+            <button onClick={handleRetrun}>Return</button> 
         </div>
     );
 };
--- 7thMilestone/module43/my-spa-route/src/components/Posts/Post.jsx
+++ 7thMilestone/module43/my-spa-route/src/components/Posts/Post.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 
 const Post = ({ post }) => {
     const { id, title, body } = post;
+    const   navigate = useNavigate();
+    const handleNavigate = () =>{
+        navigate(`/posts/${id}`)
+    }
+
     return (
         <div >
             <h3>{title}</h3>
@@ -10,6 +15,7 @@ const Post = ({ post }) => {
             <Link to={`/posts/${id}`}>
                 <button>Show Details</button>    
             </Link>
+            <button onClick={handleNavigate}>Go To Details</button>
         </div>
     );
 };
