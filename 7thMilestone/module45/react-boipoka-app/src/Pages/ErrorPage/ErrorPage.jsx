17c0b6fc28b4371f8fef97d1ef4cb85769e5925c
Sakil Mahbub <95638905+RokNRolhurley@users.noreply.github.com>
2026-08-26 10:03:53 -0400

    update45

---
+++ 7thMilestone/module45/react-boipoka-app/src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h3>This is an error</h3>
+        </div>
+    );
+};
+
+export default ErrorPage;<h3>This is an error</h3>
