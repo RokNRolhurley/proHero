0fe602e8f8ea578365c2f92e38235c5f1ad68c0b
Sakil Mahbub <sakilmahbub@gmail.com>
2026-09-16 17:16:00 -0400

    Module46

---
+++ 7thMilestone/Module46/hero-08-app/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h3>This is App JSX</h3>
+        </div>
+      </section>
+
+    </>
+  )
+}
+
+export default App
