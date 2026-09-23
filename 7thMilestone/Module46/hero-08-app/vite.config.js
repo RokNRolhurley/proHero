40e32db1040b5323abf716ee0197806f40b6095d
Sakil Mahbub <sakilmahbub@gmail.com>
2026-09-23 12:53:41 -0400

    test

--- 7thMilestone/Module46/hero-08-app/vite.config.js
+++ 7thMilestone/Module46/hero-08-app/vite.config.js
@@ -4,9 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  plugins: [react(),  tailwindcss()],
-=======
-  plugins: [react(), tailwindcss()],
->>>>>>> c45006d7030231bcb87b5b67013c7a4d8aa066ac
+
+    plugins: [react(),  tailwindcss()],
+
 })
