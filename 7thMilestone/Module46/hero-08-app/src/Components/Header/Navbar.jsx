33bf55fdfe05e0863d9b60478deff1672e027966
Sakil Mahbub <sakilmahbub@gmail.com>
2026-09-23 12:45:44 -0400

    update module 46

--- 7thMilestone/Module46/hero-08-app/src/Components/Header/Navbar.jsx
+++ 7thMilestone/Module46/hero-08-app/src/Components/Header/Navbar.jsx
@@ -4,12 +4,12 @@ const Navbar = () => {
     return (
         <div>   
             
-       {/*  <div className="navbar bg-base-100 shadow-sm">
+         <div className="navbar bg-base-100 shadow-sm">
                 <div className="navbar-start">
                     <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg aria-label="Menu" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-                    </div>
+                    </div> 
                     <ul
                         tabIndex={-1}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
@@ -24,7 +24,7 @@ const Navbar = () => {
                         <li><a>Item 3</a></li>
                     </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <a className="btn btn-ghost text-xl">Test</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
@@ -44,7 +44,7 @@ const Navbar = () => {
                 <div className="navbar-end">
                     <a className="btn">Button</a>
                 </div>
-            </div>       */}
+            </div>       
         </div>
     );
 };
